d12149ec7fdb80d4ece06f72e525befd6b4b77ee
nmdkmd <85767910+nmdkmd@users.noreply.github.com>
2022-07-27 10:38:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ SUPPORTED_COLORS = ",".join(color_codes.keys())
 COLOR_FROM_ENV = os.environ.get('APP_COLOR')
 # Generate a random color
 # COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
-COLOR = "green"
+COLOR = "blue"
 
 
 @app.route("/")
